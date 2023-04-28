c7b04716a529e49cbafddc7f94702b28b6814ea9
codeniu <codeniu@163.com>
2023-04-28 09:58:26 +0800

    update page

--- template/page.js
+++ template/page.js
@@ -1,10 +1,11 @@
-fetch("./data.json").then(async function (response) {
-  console.log(response.ok);
-  const res = await response.json();
-  const baseUrl = "https://cn.bing.com";
-  const wallpaperUrl = baseUrl + res.url;
-  console.log(wallpaperUrl);
-  let bodyElement = document.getElementsByTagName("section")[0];
-  bodyElement.style.backgroundColor = "#666";
-  bodyElement.style.backgroundImage = `url(${wallpaperUrl})`;
-});
+fetch('./data.json').then(async function (response) {
+  console.log(response.ok)
+  const res = await response.json()
+  const baseUrl = 'https://cn.bing.com'
+  const wallpaperUrl = baseUrl + res.url
+  console.log(wallpaperUrl)
+  let bodyElement = document.getElementsByTagName('section')[0]
+  bodyElement.style.backgroundColor = '#666'
+  bodyElement.style.backgroundImage = `url(${wallpaperUrl})`
+  bodyElement.innerHTML += res.title
+})
